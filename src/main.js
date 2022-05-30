cc4238c25cc647f0fcdb5f9abb66cee54cc8325f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-30 21:36:34 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+let minutes=0;
+let seconds=0;
+let tenMIllis=0;
+
+const appendTens=document.getElementById("tenMIllis");
+const appendTens=document.getElementById("seconds");
+const appendTens=document.getElementById("minutes");
