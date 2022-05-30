870e002f22c2b790366baf9caa9cba0e43e0481d
주동호 <100278301+JOODON@users.noreply.github.com>
2022-05-30 22:11:39 +0900

    05.30 스톱위치 만들기

--- src/main.js
+++ src/main.js
@@ -2,6 +2,38 @@ let minutes=0;
 let seconds=0;
 let tenMIllis=0;
 
-const appendTens=document.getElementById("tenMIllis");
-const appendTens=document.getElementById("seconds");
-const appendTens=document.getElementById("minutes");
+let appendTens=document.getElementById("tenMIllis");
+const appendSeconds=document.getElementById("seconds");
+const appendMintes=document.getElementById("minutes");
+const buttonStart=document.getElementById("bt_start");
+const buttonStop=document.getElementById("bt_stop");
+const buttonReset=document.getElementById("bt_reset");
+let intervalId;
+buttonStart.onclick=function (){
+  intervalId=setInterval(operateTimer,10);
+}
+buttonStop.onclick=function (){
+    clearInterval(intervalId);
+}
+buttonReset.onclick=function (){
+    tenMIllis=0;seconds=0;minutes=0;
+    appendTens.textContent="00"
+    appendMintes.textContent="00"
+    appendSeconds.textContent="00"
+}
+function operateTimer(){
+    tenMIllis++;
+    appendTens.textContent=tenMIllis >9 ? tenMIllis:'0'+tenMIllis;
+    if(tenMIllis > 99){
+        seconds++;
+        appendSeconds.textContent=seconds>9 ? seconds:'0'+seconds;
+        tenMIllis=0;
+        appendTens.textContent="00"
+    }
+    if(seconds>59){
+        minutes++;
+        appendMintes.textContent=minutes>9 ? minutes:'0'+minutes;
+        seconds=0
+        appendSeconds.textContent="00"
+    }
+}
